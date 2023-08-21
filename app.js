480ccf1e58b906a3c3b5d4b650d05d19a5e550fa
Timothy Theophilus H <89094785+timoothy21@users.noreply.github.com>
2023-08-21 12:56:58 +0700

    add server.js

--- app.js
+++ app.js
@@ -37,7 +37,5 @@ app.use('/api/v1/users', userRouter);
 // app.delete('/api/v1/tours/:id', deleteTour);
 
 // 4) Start Server
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
-});
+
+module.exports = app;
