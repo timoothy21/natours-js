8046917984b0142b58be81219c0d7c1d10e33a93
Timothy Theophilus H <89094785+timoothy21@users.noreply.github.com>
2023-09-02 10:49:40 +0700

    add CRUD for tour

--- app.js
+++ app.js
@@ -1,37 +1,34 @@
 const express = require('express');
 const morgan = require('morgan');
 
-const tourRouter = require('./routes/tourRoute');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// 1) Midlleware
-// console.log(process.env.NODE_ENV);
+// 1) MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
-// create middleware -> insert middleware function to stack using app.use()
-// app.use((req, res, next) => {
-//   console.log('Hello from the middleware');
-//   next();
-// });
-
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-  console.log(req.requestTime);
   next();
 });
 
-// 2) Route Handler
-
-// 3) Route => using middleware
+// 3) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-// 4) Start Server
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -1,60 +1,52 @@
 const fs = require('fs');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const Tour = require('../../models/tourModels');
+const Tour = require('./../../models/tourModel');
 
 dotenv.config({ path: './config.env' });
 
-// console.log(process.env);
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
-  process.env.DATABASE_PASSWORD,
+  process.env.DATABASE_PASSWORD
 );
 
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: false,
+    useFindAndModify: false
   })
-  .then(() => {
-    // console.log(con.connections);
-    console.log(`DB connection succesfull`);
-  });
+  .then(() => console.log('DB connection successful!'));
 
-// READ JSON File
-
+// READ JSON FILE
 const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'),
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
 );
 
 // IMPORT DATA INTO DB
 const importData = async () => {
   try {
     await Tour.create(tours);
-    console.log(`Data successfully loaded`);
-    process.exit();
+    console.log('Data successfully loaded!');
   } catch (err) {
     console.log(err);
   }
+  process.exit();
 };
 
 // DELETE ALL DATA FROM DB
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
-    console.log(`Data successfully deleted`);
-    process.exit();
+    console.log('Data successfully deleted!');
   } catch (err) {
     console.log(err);
   }
+  process.exit();
 };
 
-// node ${pathfile} --import/--delete
 if (process.argv[2] === '--import') {
   importData();
 } else if (process.argv[2] === '--delete') {
   deleteData();
 }
-
-console.log(process.argv);
